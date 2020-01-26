22c299809ad9934d0a88c3202d1cb7ca7671944d
fbenavDEV <francisco.benavides.murillo@una.cr>
2020-01-26 12:34:49 -0600

    Memoria inicializada

--- CURSO_CPP/main.cpp
+++ CURSO_CPP/main.cpp
@@ -4,12 +4,17 @@ int main()
 {
 	int numero[50];
 	int serie[50];
+	for (int k = 0; k < 50; ++k)
+	{
+		numero[k] = k;
+		serie[k] = k * 10;
+	}
 	char msj[] = "Esto es una prueba con punteros y vectores";
 	std::cout << msj << std::endl;
 	for (int k = 0; k < 50; k=k+1)
 	{
 		std::cout << numero[k] << " :: " << serie[k] << std::endl;
-		std::cout << msj[k] << std::endl;
+		std::cout << "----------------- " << std::endl;
 	}
 	std::getchar();
 }
