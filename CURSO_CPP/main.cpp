781dd91b17efc4223249400c986c11c0f9cd715a
fbenavDEV <francisco.benavides.murillo@una.cr>
2020-01-26 12:30:25 -0600

    Basura en memoria

--- CURSO_CPP/main.cpp
+++ CURSO_CPP/main.cpp
@@ -2,6 +2,14 @@
 
 int main()
 {
-	std::cout << "Hola mundo" << std::endl;	
+	int numero[50];
+	int serie[50];
+	char msj[] = "Esto es una prueba con punteros y vectores";
+	std::cout << msj << std::endl;
+	for (int k = 0; k < 50; k=k+1)
+	{
+		std::cout << numero[k] << " :: " << serie[k] << std::endl;
+		std::cout << msj[k] << std::endl;
+	}
 	std::getchar();
 }
