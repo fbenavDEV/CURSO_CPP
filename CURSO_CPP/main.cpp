8894ade627ab91d3c064002f2aa1a5a22192b0ec
fbenavDEV <francisco.benavides.murillo@una.cr>
2020-01-26 20:25:52 -0600

    Sorteo en archivo

--- CURSO_CPP/main.cpp
+++ CURSO_CPP/main.cpp
@@ -2,29 +2,33 @@
 #include <string>
 #include <iomanip>
 #include <random>
+#include <fstream>
 
-int main()
+int main(int n_args, char** args)
 {
-	const int n = 45;
-	std::mt19937 rnd;
-	std::uniform_int_distribution<int> gen_numero(0,99);
-	std::uniform_int_distribution<int> gen_serie(0, 999);
-
-	unsigned int numero[n];
-	unsigned int serie[n];
+	int n = atoi(args[2]);
+	std::string filename(args[1]);
+	std::random_device dev;
+	std::mt19937 rnd(dev());
+	std::uniform_int_distribution<> gen_numero(0,99);
+	std::uniform_int_distribution<> gen_serie(0, 999);
+	unsigned int* numero = new unsigned int[n];
+	unsigned int* serie = new unsigned int[n];
 	for (int k = 0; k < n; ++k)
 	{
 		numero[k] = gen_numero(rnd);
 		serie[k] = gen_serie(rnd);
 	}
-	std::string msj = "Sorteo de loteria, con Mersenn-Twister";
-	std::cout << msj << std::endl;
+	std::ofstream file(filename);
+	std::string msj = "Sorteo de loteria con Mersenne-Twister";
+	file << msj << std::endl;
+	file << "Numero , Serie" << std::endl;
 	for (int k = 0; k < n; ++k)
 	{
-		std::cout << "Numero: " << std::setw(2) << std::setfill('0') << numero[k];
-		std::cout << "   Serie: " << std::setw(3) << std::setfill('0') << serie[k];
-		std::cout << std::endl;
-		//printf("Numero: %02d, Serie: %03d ", numero[k], serie[k]);
+		file << std::setw(2) << std::setfill('0') << numero[k] << ",";
+		file << std::setw(3) << std::setfill('0') << serie[k];
+		file << std::endl;
 	}
-	std::getchar();
+	file.close();
+	return(0);
 }
