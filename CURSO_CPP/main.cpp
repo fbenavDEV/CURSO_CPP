07251e11811963ff5c669f77eb7b29267f440200
fbenavDEV <francisco.benavides.murillo@una.cr>
2020-01-26 19:13:04 -0600

    Generador de sorteos (numeros aleatorios)

--- CURSO_CPP/main.cpp
+++ CURSO_CPP/main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <random>
 
 int main()
 {
-	int n = 50;
-	int numero[n];
-	int serie[n];
+	const int n = 45;
+	std::mt19937 rnd;
+	std::uniform_int_distribution<int> gen_numero(0,99);
+	std::uniform_int_distribution<int> gen_serie(0, 999);
+
+	unsigned int numero[n];
+	unsigned int serie[n];
 	for (int k = 0; k < n; ++k)
 	{
-		numero[k] = k;
-		serie[k] = k * 10;
+		numero[k] = gen_numero(rnd);
+		serie[k] = gen_serie(rnd);
 	}
-	char msj[] = "Esto es una prueba con punteros y vectores";
+	std::string msj = "Sorteo de loteria, con Mersenn-Twister";
 	std::cout << msj << std::endl;
-	for (int k = 0; k < 50; k=k+1)
+	for (int k = 0; k < n; ++k)
 	{
-		std::cout << numero[k] << " :: " << serie[k] << std::endl;
-		std::cout << "----------------- " << std::endl;
+		std::cout << "Numero: " << std::setw(2) << std::setfill('0') << numero[k];
+		std::cout << "   Serie: " << std::setw(3) << std::setfill('0') << serie[k];
+		std::cout << std::endl;
+		//printf("Numero: %02d, Serie: %03d ", numero[k], serie[k]);
 	}
 	std::getchar();
 }
